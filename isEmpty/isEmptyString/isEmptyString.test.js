429a68b943b658bea0c72fe33b880e6fade642fa
Suissa <jnascimento@gmail.com>
2016-03-05 08:19:03 -0300

    Agora com testes LOL

--- isEmpty/isEmptyString/isEmptyString.test.js
+++ isEmpty/isEmptyString/isEmptyString.test.js
@@ -2,11 +2,11 @@
 
 const assert = require('assert');
 
-const valueFALSE = 12;
-const valueTRUE = 'suissa';
+const valueFALSE = 'Suissera';
+const valueTRUE = '';
 
-assert.equal(true, require('./isOnlyLetters')(valueTRUE));
-assert.equal(false, require('./isOnlyLetters')(valueFALSE));
+assert.equal(true, require('./isEmptyString')(valueTRUE));
+assert.equal(false, require('./isEmptyString')(valueFALSE));
 
-console.log(valueTRUE + ' é apenas letras?', require('./isOnlyLetters')(valueTRUE));
-console.log(valueFALSE + ' é apenas letras?', require('./isOnlyLetters')(valueFALSE));
+console.log(valueTRUE + ' é uma String vazia?', require('./isEmptyString')(valueTRUE));
+console.log(valueFALSE + ' é uma String vazia?', require('./isEmptyString')(valueFALSE));
