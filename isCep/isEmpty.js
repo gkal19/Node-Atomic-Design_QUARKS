35cde6f9b3a3da307e29215b2e07c6cfa8041d16
carlosmachel <carlosmachel@outlook.com>
2016-03-06 08:57:58 -0300

    modificacao no arquivo isEmpty.js

--- isCep/isEmpty.js
+++ isCep/isEmpty.js
@@ -1,5 +1,5 @@
 'use strict';
-//falta colocar o isEmpty em uma pasta separada e remover os que já tem.
+//falta colocar o isEmpty em uma pasta separada e remover os outros isEmpty que já tem.
 
 module.exports = (value) => {
   if (value === null || value === undefined) return true;
