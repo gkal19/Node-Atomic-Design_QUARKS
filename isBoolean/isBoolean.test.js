56f68e09a2c40a019f23f1730ea7132adbf5b430
Carlos Machel <carlosmachel@outlook.com>
2016-03-16 00:22:32 -0300

    Testes alterados de isBoolean. Feito de isInObject e isObject

--- isBoolean/isBoolean.test.js
+++ isBoolean/isBoolean.test.js
@@ -1,12 +1,35 @@
 'use strict';
 
-const assert = require('assert');
+const expect = require('chai').expect;
 
-const valueTRUE = false;
-const valueFALSE = '1';
+//const assert = require('assert');
 
-assert.equal(true, require('./isBoolean')(valueTRUE));
-assert.equal(false, require('./isBoolean')(valueFALSE));
+const valueTRUE = [false, true];
+const valueFALSE = ['1', undefined, null, 0, new Date()];
 
-console.log(valueTRUE + ' é um Boolean?', require('./isBoolean')(valueTRUE));
-console.log(valueFALSE + ' é um Boolean?', require('./isBoolean')(valueFALSE));
+const testFalse = (values) => {
+  values.forEach((element,index) =>{
+    it('Testando: ' + element, () =>{
+      expect(require('./isBoolean')(element)).to.equal(false);
+    });
+  });
+};
+
+const testTrue = (values) => {
+  values.forEach((element,index) => {
+    it('Testando: ' + element, ()=>{
+      expect(require('./isBoolean')(element)).to.equal(true);
+    });
+  });
+};
+
+describe('Teste de boolean', ()=>{
+  describe('boleanos inválidos: ', () => testFalse(valueFALSE));
+  describe('boleanos válidos: ', () => testTrue(valueTRUE));
+});
+
+//assert.equal(true, require('./isBoolean')(valueTRUE));
+//assert.equal(false, require('./isBoolean')(valueFALSE));
+
+//console.log(valueTRUE + ' é um Boolean?', require('./isBoolean')(valueTRUE));
+//console.log(valueFALSE + ' é um Boolean?', require('./isBoolean')(valueFALSE));
